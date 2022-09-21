339089969847830302a8f646a01138d3e2478901
Ahmed Magdy <ahmadmgdi94@gmail.com>
2022-09-21 20:35:59 +0200

    Sidekiq Workers

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,6 +1,10 @@
 class Website < ApplicationRecord
   validates :name, presence: true
   validates :url, presence: true
-  URL_REGEXP = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
+  URL_REGEXP = %r{\A(http|https)://[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}ix
   validates :url, format: { with: URL_REGEXP, message: 'You provided invalid URL' }
+
+  def enqueu_status_check
+    WebsiteStatusWorker.perform_async(id)
+  end
 end
