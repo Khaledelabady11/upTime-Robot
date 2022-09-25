2951fe5ab84421ad154e97bd5af09c0daa1d3c84
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2022-09-26 00:26:20 +0200

    Scraped Details of Btech

---
+++ app/services/btech_scrap_details.rb
@@ -0,0 +1,37 @@
+class BtechScrapDetails
+    URL_REGEXP = %r{\A(http|https)://[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}ix
+
+    def initialize(url = 'https://btech.com/ar/xiaomi-redmi-10c-dual-sim-128gb-4gb-4g-lte-grey.html')
+      @url = url
+    end
+
+    def perform
+        @document = fetch_document
+        fetch_product_title
+        fetch_product_price
+        fetch_product_images
+        # @document.search('.product-item-view a').text.strip
+    end
+
+    def fetch_product_title
+        byebug
+        @document.search('.base').text.strip
+    end
+
+    def fetch_product_price
+        @document.search('.price-huge-static').text.strip
+    end
+
+    def fetch_product_images
+        images = @document.search('.MagicToolboxMainContainer img')
+    end
+  
+    def fetch_document
+        mechanize_agent = Mechanize.new
+        mechanize_agent.user_agent_alias = 'Mac Safari'
+        @document = mechanize_agent.get(@url)
+        @document.encoding = 'utf-8'
+        @document
+    end
+end
+  
